ad83f45205aaf84d92f13b28a6bed2fae1f4ba21
EEw33 <arlannur2712@gmail.com>
2026-01-18 20:01:36 +0500

    gitignore modified, Member table edited

--- src/edu/aitu/oop3/entities/Member.java
+++ src/edu/aitu/oop3/entities/Member.java
@@ -4,37 +4,62 @@ public class Member {
     private long id;
     private String name;
     private String email;
+    private Long membershipTypeId;
+    private String membershipStart;
+    private String membershipEnd;
 
     public Member() {
     }
 
-    public Member(long id, String name, String email) {
+    public Member(long id, String name, String email,
+                  Long membershipTypeId,
+                  String membershipStart,
+                  String membershipEnd) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.membershipTypeId = membershipTypeId;
+        this.membershipStart = membershipStart;
+        this.membershipEnd = membershipEnd;
     }
+
+
+
 
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
+    }
+    public Long getMembershipTypeId() {
+        return membershipTypeId;
+    }
+    public void setMembershipTypeId(Long membershipTypeId) {
+        this.membershipTypeId = membershipTypeId;
+    }
+    public String getMembershipStart() {
+        return membershipStart;
+    }
+    public void setMembershipStart(String membershipStart) {
+        this.membershipStart = membershipStart;
+    }
+    public String getMembershipEnd() {
+        return membershipEnd;
+    }
+    public void setMembershipEnd(String membershipEnd) {
+        this.membershipEnd = membershipEnd;
     }
 }
